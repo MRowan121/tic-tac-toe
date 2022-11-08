5470324f837ebba40e34f1453ec9de1603f3a6fc
Matt Rowan <matt.rowan121@gmail.com>
2022-11-08 13:53:44 -0500

    Add first pass of increase wins method

--- player.js
+++ player.js
@@ -4,6 +4,12 @@ class Player {
         this.token = playerDetails.token;
         this.wins = playerDetails.wins || 0;
     };
+
+    increaseWins() {
+        // Will need to come back and add if statement
+        // if player wins, increase win count. Else, do nothing.
+        this.wins = this.wins + 1;
+    };
 };
 
 
