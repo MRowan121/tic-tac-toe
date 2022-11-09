feb678d053d582f17c3fb0e62121a6fde5f0ae41
Matt Rowan <matt.rowan121@gmail.com>
2022-11-09 13:05:58 -0500

    Add function to update game players property

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var Player = require('./player')
+
 class Game {
     constructor() {
         this.players = [];
@@ -14,5 +16,11 @@ class Game {
             [0, 4, 8],
             [2, 4, 6],
         ];
-    }
-}
+    };
+
+    addPlayers(playerDetails) {
+        this.players.push(playerDetails)
+    };
+};
+
+module.exports = Game;
