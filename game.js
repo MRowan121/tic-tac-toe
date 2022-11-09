79fa6b4a0962bc21d580f0711819ad7df5e5cb16
Matt Rowan <matt.rowan121@gmail.com>
2022-11-09 13:00:30 -0500

    Add game class

---
+++ game.js
@@ -0,0 +1,18 @@
+class Game {
+    constructor() {
+        this.players = [];
+        this.gameNumber = 1;
+        this.turnNumber = 1;
+        this.gameBoard = ['','','','','','','','',''];
+        this.winning = [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+    }
+}
