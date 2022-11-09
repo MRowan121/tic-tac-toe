d4ebf838b643b34c2fc1999fa48545ab0417b635
Matt Rowan <matt.rowan121@gmail.com>
2022-11-09 13:44:34 -0500

    Add reset board funcitonality

--- game.js
+++ game.js
@@ -48,6 +48,7 @@ class Game {
             && activePlayer.tokenPlacement.includes(this.winning[i][1]) 
             && activePlayer.tokenPlacement.includes(this.winning[i][2])) {
                 activePlayer.increaseWins();
+                this.resetBoard();
                 console.log(`${activePlayer.id} Wins!`)
             };
         };
@@ -55,9 +56,30 @@ class Game {
 
     checkForDraw() {
         if(this.turnNumber === 10) {
+            this.resetBoard();
             console.log(`DRAW!`)
         };
     };
+
+    resetBoard() {
+        this.gameNumber = this.gameNumber + 1;
+        for(var i = 0; i < this.players.length; i++) {
+            if(this.gameNumber > 3) {
+                this.gameNumber = 1;
+                this.turnNumber = 1;
+                this.players[i].tokenPlacement = [];
+                // Need to understand why line 71 isn't clearing out both players wins.
+                // this.players[i].wins = 0;
+                this.players[0].wins = 0;
+                this.players[1].wins = 0;
+                this.gameBoard = ['','','','','','','','',''];
+            } else {
+                this.turnNumber = 1;
+                this.players[i].tokenPlacement = [];
+                this.gameBoard = ['','','','','','','','',''];
+            }
+        }
+    }
 };
 
 module.exports = Game;
